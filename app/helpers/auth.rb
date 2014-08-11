84962011c60a7ea7e08bd01dd6c252effe053a59
Luchs <Luchs@euirc.eu>
2014-08-11 18:07:24 +0200

    Add current_user helper function

--- app/helpers/auth.rb
+++ app/helpers/auth.rb
@@ -5,6 +5,12 @@ module MvwIntern
     def login?
       !session[:email].nil?
     end
+
+    def current_user
+      if login?
+        Models::User[email: session[:email]]
+      end
+    end
   
   end
 end
