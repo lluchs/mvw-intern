fa45f6e2e0f150af23a36c65a8ea0d65ff104e34
Luchs <Luchs@euirc.eu>
2014-08-26 22:43:55 +0200

    Use prev/next buttons instead of input fields

--- calendar.js
+++ calendar.js
@@ -5,8 +5,7 @@
   var Calendar = Ractive.extend({
     template: '#template',
     data: {
-      month: 8,
-      year: 2014,
+      month: moment().startOf('month'),
       weekdays: (function() {
         return [0, 1, 2, 3, 4, 5, 6].map(function(i) {
           return moment().weekday(i).format('dd');
@@ -15,32 +14,42 @@
     },
     computed: {
       monthname: function() {
-        var month, year;
+        var month;
         month = this.get('month');
-        year = this.get('year');
-        return moment([year, month]).format('MMMM YYYY');
+        return month.format('MMMM YYYY');
       },
       weeks: function() {
-        var month, year, m, weeks, days;
+        var month, m, weeks, days;
         month = this.get('month');
-        year = this.get('year');
         // Start with the first day of the given month.
-        m = moment([year, month]);
+        m = month.clone().startOf('month');
         // Go back to the first day of the week.
         while (m.weekday()) m.subtract(1, 'day');
         weeks = [];
-        while (m.isBefore(moment([year, month]).add(1, 'month'))) {
+        while (m.isBefore(month.clone().add(1, 'month'))) {
           days = [];
           for (var cmp = m.clone().add(1, 'week'); m.isBefore(cmp); m.add(1, 'day')) {
             days.push({
               day: m.date(),
-              outside: m.month() != month,
+              outside: m.month() != month.month(),
             });
           }
           weeks.push({days: days});
         }
         return weeks;
       },
+    },
+    init: function() {
+      this.on({
+        'prev-month': function() {
+          this.get('month').subtract(1, 'month');
+          this.update('month');
+        },
+        'next-month': function() {
+          this.get('month').add(1, 'month');
+          this.update('month');
+        },
+      });
     },
   });
 
