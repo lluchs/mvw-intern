ac7dcb673d9b67232a4d27d88568acb97b032e77
Luchs <Luchs@euirc.eu>
2015-05-08 18:18:58 +0200

    Fix dates not being passed as ISO8601

--- app/models.rb
+++ app/models.rb
@@ -3,5 +3,8 @@ require 'sequel'
 # Enable model.to_json
 Sequel::Model.plugin :json_serializer
 
+# Use DateTime as it formats to ISO8601
+Sequel.datetime_class = DateTime
+
 require './app/models/user'
 require './app/models/event'
