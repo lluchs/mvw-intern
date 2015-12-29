e2d4b8871c0530f9773e11006b780e2cd1535fa3
Lukas Werling <lukas.werling@gmail.com>
2015-12-29 17:53:55 +0100

    Add password reset for admins

--- app/helpers/user.rb
+++ app/helpers/user.rb
@@ -12,12 +12,27 @@ module MvwIntern
       end
     end
   
+    # Updates a user object from form parameters.
     def update_user_params(user, params)
       user.name       = params[:name]
       user.email      = params[:email]
       user.instrument = params[:instrument]
       user.birthday   = params[:birthday]
       user.admin      = params[:admin]
+
+      if params[:reset_password] == 'on'
+        reset_user_password(user)
+      end
+    end
+
+    # Resets a user's password to their birthday.
+    def reset_user_password(user)
+      if user.birthday.nil?
+        # No birthday => delete password
+        user.password = nil
+      else
+        user.password = user.birthday.strftime('%d.%m.%Y')
+      end
     end
 
   end
