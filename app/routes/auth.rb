20292f46a23307318c8902667690b58ab64af9dc
Lukas Werling <lukas@lwrl.de>
2019-05-21 20:45:27 +0200

    Add Discourse SSO endpoint

--- app/routes/auth.rb
+++ app/routes/auth.rb
@@ -53,6 +53,47 @@ module MvwIntern
           redirect "#{url}##{token}"
         end
 
+        # Discourse SSO endpoint
+        app.get "/sso" do
+          sso = params['sso']
+          sig = params['sig']
+          if sso.nil? || sig.nil? || OpenSSL::HMAC.hexdigest("sha256", settings.hmac_secret, sso) != sig
+            halt 400, "Invalid SSO payload"
+            return
+          else
+            base64_sso = URI.unescape(sso)
+            decoded_sso = Base64.decode64(base64_sso)
+            params = Rack::Utils.parse_nested_query(decoded_sso)
+            nonce = params['nonce']
+            return_sso_url = URI(params['return_sso_url'])
+
+            user = Models::User.by_email(session[:email])
+            groups = user.groups.map(&:name)
+            payload = {
+              "nonce" => nonce,
+              "name" => user.name,
+              "external_id" => user.id,
+              "email" => user.email,
+              "username" => user.name,
+              "groups" => groups.join(','),
+            }
+
+            payload["admin"] = true if user.admin
+
+            payload = Rack::Utils.build_query(payload)
+            base64_payload = Base64.strict_encode64(payload)
+            encoded_payload = URI.escape(base64_payload)
+            new_sig = OpenSSL::HMAC.hexdigest("sha256", settings.hmac_secret, base64_payload)
+            q = "sso=#{encoded_payload}&sig=#{new_sig}"
+            if return_sso_url.query.nil?
+              return_sso_url.query = q
+            else
+              return_sso_url.query += "&#{q}"
+            end
+            redirect return_sso_url.to_s
+          end
+        end
+
       end
     end
   end
